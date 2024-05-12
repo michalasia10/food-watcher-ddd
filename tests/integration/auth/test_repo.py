de750254f81ba43a61bec80608fdceb21e730ebc
michalasia10 <misiek1027@o2.pl>
2024-05-12 22:40:03 +0200

    refactor: update rep test

--- tests/integration/auth/test_repo.py
+++ tests/integration/auth/test_repo.py
@@ -66,3 +66,33 @@ async def test_create_multiple_and_get_all():
         assert user.first_name in allowed_first_names
         assert user.created_at is not None
         assert user.updated_at is not None
+
+
+@pytest.mark.asyncio
+async def test_update_user():
+    # given
+    email = "test@domain.com"
+    user_to_update = User.create(
+        email=email,
+        password="test",
+        username="test",
+        first_name="test",
+        last_name="test",
+        status=StatusEnum.ACTIVE.value,
+        type=TypeEnum.USER.value
+    )
+    await UserTortoiseRepo.asave(entity=user_to_update)
+
+    # when
+    new_username = "new_username"
+    new_first_name = "new_first_name"
+    user_to_update.username = new_username
+    user_to_update.first_name = new_first_name
+    await UserTortoiseRepo.aupdate(entity=user_to_update)
+
+    # then
+    user_get_from_db = await UserTortoiseRepo.aget_first_from_filter(email=email)
+    assert user_get_from_db.username == new_username
+    assert user_get_from_db.first_name == new_first_name
+    assert user_get_from_db.updated_at > user_get_from_db.created_at
+    assert user_get_from_db.email == user_get_from_db.email
